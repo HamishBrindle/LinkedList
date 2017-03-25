e83c4af6ed472bbb25c36f02a7fb702423af2b2f
HamishBrindle <hamishbrindle@gmail.com>
2017-03-25 13:46:36 -0700

    First commit.

---
+++ src/main/LinkedList.java
@@ -0,0 +1,173 @@
+package main;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.ListIterator;
+
+/**
+ * LinkedList creates list of E elements.
+ * @author Hamish
+ * @date 2017-03-25
+ */
+public class LinkedList<E> implements Collection<E> {
+
+    private Node head;
+
+    private class Node {
+        public E data;
+        public Node previous;
+        public Node next;
+
+        /**
+         * Constructs Node.
+         * @param e Element
+         */
+        public Node(E e) {
+            data = e;
+        }
+    }
+
+    @Override
+    public boolean add(E e) {
+
+        // Node is empty
+        if (isEmpty()) {
+            head = new Node(e);
+            return true;
+        }
+
+        // Node already has element
+
+
+        return false;
+    }
+
+    @Override
+    public int size() {
+
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return (head == null);
+    }
+
+    @Override
+    public boolean contains(Object o) {
+
+        return false;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            @Override
+            public boolean hasNext() {
+                return false;
+            }
+
+            @Override
+            public E next() {
+                return null;
+            }
+        };
+    }
+
+    public ListIterator<E> listIterator(int index) {
+        return new ListIterator<E>() {
+            @Override
+            public boolean hasNext() {
+                return false;
+            }
+
+            @Override
+            public E next() {
+                return null;
+            }
+
+            @Override
+            public boolean hasPrevious() {
+                return false;
+            }
+
+            @Override
+            public E previous() {
+                return null;
+            }
+
+            @Override
+            public int nextIndex() {
+                return 0;
+            }
+
+            @Override
+            public int previousIndex() {
+                return 0;
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            public void set(E e) {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            public void add(E e) {
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
+
+    @Override
+    public Object[] toArray() {
+
+        return new Object[0];
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+
+        return null;
+    }
+
+
+    @Override
+    public boolean remove(Object o) {
+
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+}
